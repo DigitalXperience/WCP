bf3ec2a5d4f46b434b90bda62a7a85e901b3f8ca
Patient Assontia <assontia@gmail.com>
2018-04-29 21:42:30 +0100

    Formulaire de rencontre

--- application/views/rencontre_new.php
+++ application/views/rencontre_new.php
@@ -57,25 +57,71 @@
 
 		<form role="form" action="<?php echo base_url().'index.php/rencontres/ajouter'; ?>" method="post" data-parsley-validate>
 	  <div class="card pd-20 pd-sm-40">
-          <h6 class="card-body-title">Nouvelle Equipe</h6>
-          <p class="mg-b-20 mg-sm-b-30">Entrez les informations de l'équipe</p>
+          <h6 class="card-body-title">Nouvelle Rencontre</h6>
+          <p class="mg-b-20 mg-sm-b-30">Entrez les informations de la rencontre</p>
 			<div class="form-layout">
 			  <div class="row mg-b-25">
-				<div class="col-lg-8">
+				<div class="col-lg-4">
 					<div class="form-group mg-b-10-force">
-						<label class="form-control-label">Entrez son nom : <span class="tx-danger">*</span></label>
-					  <input class="form-control" name="name" value="<?php if(isset($current)) echo $current->name; ?>" placeholder="Nom de l'équipe" type="text" required />
+					  <label class="form-control-label">Equipe 1 : <span class="tx-danger">*</span></label>
+					  <select class="form-control select2" name="equipe_id1" data-placeholder="Sélectionnez l'équipe 1">
+						<option label="Sélectionnez l'équipe 1"></option>
+							<?php foreach ($lstEquipes as $equipe) { 
+								echo "<option value='".$equipe->id."'>".$equipe->name."</option>";           	
+							} ?>
+					  </select>
 					</div>
-				</div><!-- col -->
-				<div class="col-lg-8">
+              </div>
+			  <div class="col-lg-4">
 					<div class="form-group mg-b-10-force">
-						<label class="form-control-label">Entrez le lien vers le drapeau : <span class="tx-danger">*</span></label>
-						  <input type="text" class="form-control" value="<?php if(isset($current)) echo $current->flag; ?>" name="flag" placeholder="lien vers le drapeau" required />
+					  <label class="form-control-label">Equipe 2 : <span class="tx-danger">*</span></label>
+					  <select class="form-control select2" name="equipe_id2" data-placeholder="Sélectionnez l'équipe 2">
+						<option label="Sélectionnez l'équipe 2"></option>
+							<?php foreach ($lstEquipes as $equipe) { 
+								echo "<option value='".$equipe->id."'>".$equipe->name."</option>";           	
+							} ?>
+					  </select>
 					</div>
+              </div>
+			  <div class="col-lg-4">
+					
 				</div><!-- col -->
-				<?php if(isset($current)) echo '<input type="hidden" name="id" value="'.$current->id.'">'; ?>
+				<div class="col-lg-4">
+					<div class="form-group mg-b-10-force">
+						<p class="mg-b-10">Date de la rencontre</p>
+						<div class="wd-200">
+							<div class="input-group">
+							  <span class="input-group-addon"><i class="icon ion-calendar tx-16 lh-0 op-6"></i></span>
+							  <input type="text" id="r_date" name="r_date" class="form-control fc-datepicker" placeholder="JJ/MM/AAAA">
+							</div>
+						  </div>
+					  </div>
+				</div><!-- col -->
+				<div class="col-lg-4">
+					<div class="form-group">
+					  <label class="form-control-label">Heure de la recontre : <span class="tx-danger">*</span></label>
+					  <input class="form-control" type="text" name="r_heure" value="" placeholder="HH:MM">
+					</div>
+				</div><!-- col-4 -->
 			  </div>
+			  
+			  <div class="row mg-t-10">
+					<div class="col-lg-3">
+					  <label class="rdiobox">
+						<input name="rdio" type="radio">
+						<span>Mettre en avant</span>
+					  </label>
+					</div><!-- col-3 -->
+					<div class="col-lg-3 mg-t-20 mg-lg-t-0">
+					  <label class="rdiobox">
+						<input name="rdio" type="radio" checked>
+						<span>Ne pas mettre en avant</span>
+					  </label>
+					</div>
+				  </div><!-- row -->
 			  <div class="form-layout-footer">
+					
+                    <?php if(isset($current)) echo '<input type="hidden" name="id" value="'.$current->id.'">'; ?>
 				  <button type="submit" class="btn btn-info mg-r-5">Enregistrer</button>
 				  <button type="reset" class="btn btn-secondary">Annuler</button>
 				</div>
@@ -145,6 +191,7 @@
                            </div>
 
                        </div>
+					    
                   </div><!-- /.box-body -->
 
                   <div class="box-footer">
@@ -179,13 +226,26 @@
 
     <script type="text/javascript">
       $(function () {
-        $('#r_date').datepicker({
+        /*$('#r_date').datepicker({
           showOtherMonths: false,
           selectOtherMonths: false,
           format: 'yyyy-mm-dd',
           numberOfMonths: 2
+        });*/
+		// Datepicker
+        $('.fc-datepicker').datepicker({
+          showOtherMonths: true,
+          selectOtherMonths: true
         });
       });
+	  
+	  $(function(){
+        'use strict';
+
+        $('.select2').select2({
+          minimumResultsForSearch: Infinity
+        });
+      })
     </script>
 
   </body>

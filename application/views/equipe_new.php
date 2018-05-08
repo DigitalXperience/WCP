0a653acd261e9d97f606345755f70c08919da645
atchopba <atchopba@gmail.com>
2018-05-09 00:14:28 +0200

    Suppression menu recherche
    Suppression du menu recherche dans la gestion des équipes, des utilisateurs et des pronostics

--- application/views/equipe_new.php
+++ application/views/equipe_new.php
@@ -73,12 +73,7 @@
 	
 	<div class="am-pagetitle">
       <h5 class="am-title">Form Elements</h5>
-      <form id="searchBar" class="search-bar" action="index.html">
-        <div class="form-control-wrapper">
-          <input type="search" class="form-control bd-0" placeholder="Search...">
-        </div><!-- form-control-wrapper -->
-        <button id="searchBtn" class="btn btn-orange"><i class="fa fa-search"></i></button>
-      </form><!-- search-bar -->
+      
     </div><!-- am-pagetitle -->
 
     <div class="am-mainpanel">
